495b1cc190efd4f85f20005d3ce7be4e9514ffa2
jarray massaoud <jarraymassoud@gmail.com>
2026-08-12 01:17:26 +0100

    modifier logo

--- src/data.js
+++ src/data.js
@@ -1,73 +1,35 @@
-export const sliderItems = [
-  {
-    id: 1,
-    img: "images/img1.jpg",
-    title: "Winter Flower ",
-    desc: "",
-    bg: "fcf1ed",
-  },
+import img1 from "./assets/images/img1.jpg";
+import img25 from "./assets/images/img25.jpg";
+import img20 from "./assets/images/img20.jpg";
+import img4 from "./assets/images/img4.jpg";
+import img5 from "./assets/images/img5.jpg";
+import img6 from "./assets/images/img6.jpg";
+import img7 from "./assets/images/img7.jpg";
+import img8 from "./assets/images/img8.jpg";
+import img10 from "./assets/images/img10.jpg";
+import img11 from "./assets/images/img11.jpg";
+import img13 from "./assets/images/img13.jpg";
+import img14 from "./assets/images/img14.jpg";
+import img26 from "./assets/images/img26.jpg";
 
-  {
-    id: 2,
-    img: "images/img25.jpg",
-    title: "Popular Flower ",
-    desc: "",
-    bg: "f5fafd",
-  },
-  {
-    id: 3,
-    img: "images/img20.jpg",
-    title: " Spring Flower ",
-    desc: "",
-    bg: "fbf0f4",
-  },
+export const sliderItems = [
+  { id: 1, img: img1, title: "Winter Flower", desc: "", bg: "fcf1ed" },
+  { id: 2, img: img25, title: "Popular Flower", desc: "", bg: "f5fafd" },
+  { id: 3, img: img20, title: "Spring Flower", desc: "", bg: "fbf0f4" },
 ];
 
 export const categories = [
-  {
-    id: 1,
-    img: "images/img4.jpg",
-    title: " Spring Flower",
-  },
-  {
-    id: 2,
-    img: "images/img5.jpg",
-    title: " Summer Flower",
-  },
-  {
-    id: 3,
-    img: "images/img6.jpg",
-    title: " Popular Flower",
-  },
+  { id: 1, img: img4, title: "Spring Flower" },
+  { id: 2, img: img5, title: "Summer Flower" },
+  { id: 3, img: img6, title: "Popular Flower" },
 ];
-export const products = [
-  {
-    id: 1,
-    img: "images/img7.jpg",
-  },
-  {
-    id: 2,
-    img: "images/img8.jpg",
-  },
-  {
-    id: 3,
-    img: "images/img10.jpg",
-  },
-  {
-    id: 4,
-    img: "images/img11.jpg",
-  },
-  {
-    id: 5,
-    img: "images/img13.jpg",
-  },
 
-  {
-    id: 6,
-    img: "images/img14.jpg",
-  },
-  {
-    id: 6,
-    img: "images/img26.jpg",
-  },
+export const products = [
+  { id: 1, img: img7 },
+  { id: 2, img: img8 },
+  { id: 3, img: img10 },
+  { id: 4, img: img11 },
+  { id: 5, img: img13 },
+  { id: 6, img: img14 },
+  { id: 7, img: img26 }, // ⚠️صلحنا الـ ID المكرر
 ];
